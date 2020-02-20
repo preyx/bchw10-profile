33a22b8cc7ff905e3d1027527945312ccbd08ca8
Bryan Y <xpreyx@gmail.com>
2020-02-19 22:39:58 -0800

    final draft

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# bchw10-profile
+
+<a href="https://github.com/preyx" style="float:right"><img src="https://avatars1.githubusercontent.com/u/425908?v=4" alt="preyx" title="preyx" width="120" height="120"></a>
+
+# BCHW10-PROFILE
+
+![License: None](https://img.shields.io/badge/License-None-brightgreen)
+
+_Repo by preyx_
+
+__Team Profile Generator__
+
+---
+
+__Installation:__
+None
+
+__Usage:__
+Run: node app.js
+
+__Contributors:__
+None
+
+__Tests:__
+Constructors: Passed
+
+__Questions:__
+None
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const { JSDOM } = require('jsdom')
 const prompt = require('inquirer').createPromptModule()
 
 const Manager = require('./lib/Manager')
@@ -6,94 +7,98 @@ const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 
 const init = async _ => {
-  let more = false
-  const eInput = {}
+  let eInput = {}
+  let output = ''
+  let ans = ''
+  let eList = []
   const ePrompt = {
     Manager: 'Office Number:',
     Engineer: 'GitHub Username:',
     Intern: 'School:'
   }
-  const eList = []
   // const n = Date.now()
   do {
-    try {
-      const { ans } = await prompt(
-        {
-          type: 'list',
-          name: 'eType',
-          message: 'Employee type:',
-          choices: ['Manager', 'Engineer', 'Intern']
-        })
-      eInput.type = ans.eType
-    } catch (e) { console.error(e) }
-    try {
-      const { ans } = await prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: 'Employee name:'
-        },
-        {
-          type: 'number',
-          name: 'id',
-          message: 'Employee ID:'
-        },
-        {
-          type: 'number',
-          name: 'email',
-          message: 'Employee Email:'
-        },
-        {
-          type: 'input',
-          name: 'extra',
-          message: ePrompt.eType
-        }])
-      eInput.name = ans.name
-      eInput.id = ans.id
-      eInput.email = ans.email
-    } catch (e) { console.error(e) }
-    if (eInput.type !== 'Employee') {
-      try {
-        const { ans } = await prompt(
-          {
-            type: 'input',
-            name: 'extra',
-            message: ePrompt.eType
-          })
-        eInput.extra = ans.extra
-      } catch (e) { console.error(e) }
-    }
+    ans = await prompt(
+      {
+        type: 'list',
+        name: 'type',
+        message: 'Employee type:',
+        choices: ['Manager', 'Engineer', 'Intern']
+      })
+    eInput.type = ans.type
+    ans = await prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Employee name:'
+      },
+      {
+        type: 'number',
+        name: 'id',
+        message: 'Employee ID:'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Employee Email:'
+      },
+      {
+        type: 'input',
+        name: 'extra',
+        message: ePrompt[eInput.type]
+      }])
+    eInput.name = ans.name
+    eInput.id = ans.id
+    eInput.email = ans.email
+    eInput.extra = ans.extra
     switch (eInput.type) {
       case 'Manager':
-        eList.push(Manager(eInput.name, eInput.id, eInput.email, eInput.extra))
+        eList.push(new Manager(eInput.name, eInput.id, eInput.email, eInput.extra))
         break
       case 'Engineer':
-        eList.push(Engineer(eInput.name, eInput.id, eInput.email, eInput.extra))
+        eList.push(new Engineer(eInput.name, eInput.id, eInput.email, eInput.extra))
         break
       case 'Intern':
-        eList.push(Intern(eInput.name, eInput.id, eInput.email, eInput.extra))
+        eList.push(new Intern(eInput.name, eInput.id, eInput.email, eInput.extra))
         break
     }
     console.log('Employee successfully added!')
-    try {
-      const { ans } = await prompt(
-        {
-          type: 'confirm',
-          name: 'more',
-          message: 'Any more employees?'
-        })
-      more = ans.more
-    } catch (e) { console.error(e) }
-  } while (more)
-  eList.forEach(elem => {
-    switch (elem.getRole()) {
-      case 'Manager':
-        break
-      case 'Engineer':
-        break
-      case 'Intern':
-        break
-    }
+    ans = await prompt(
+      {
+        type: 'confirm',
+        name: 'more',
+        message: 'Any more employees?'
+      })
+  } while (ans.more)
+  await eList.forEach(elem => {
+    fs.readFile(`./templates/${elem.getRole()}.html`, 'utf8', (e, data) => {
+      if (e) { console.log(e) }
+      const { document } = (new JSDOM(data)).window
+      document.getElementById('eName').textContent = elem.getName()
+      document.getElementById('eId').textContent = elem.getId()
+      document.getElementById('eEmail').innerHTML = `<a href="mailto:${elem.getEmail()}">${elem.getEmail()}</a>`
+      switch (elem.getRole()) {
+        case 'Manager':
+          document.getElementById('eExtra').textContent = elem.getOfficeNumber()
+          break
+        case 'Engineer':
+          document.getElementById('eExtra').innerHTML = `<a href="https://github.com/${elem.getGithub()}">${elem.getGithub()}</a>`
+          break
+        case 'Student':
+          document.getElementById('eExtra').textContent = elem.getSchool()
+          break
+      }
+      output += document.documentElement.outerHTML
+    })
+  })
+  fs.readFile('./templates/main.html', 'utf8', async (e, data) => {
+    console.log(output)
+    if (e) { console.log(e) }
+    const { document } = (new JSDOM(data)).window
+    document.getElementById('eMain').innerHTML = output
+    fs.writeFile('./output/team.html', document.documentElement.outerHTML, e => {
+      if (e) { console.log(e) }
+    })
   })
 }
 
